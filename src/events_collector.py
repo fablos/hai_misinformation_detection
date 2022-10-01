af405e6434db9aa47c5a193042f574bfc6f5c687
Fabrizio Lo Scudo <22591699+fablos@users.noreply.github.com>
2022-10-01 23:08:11 +0200

    Update events_collector.py

--- src/events_collector.py
+++ src/events_collector.py
@@ -1,2 +1,9 @@
+import requests
 
 source = 'https://www.nytimes.com/article/ukraine-russia-war-timeline.html'
+
+headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
+
+response = requests.get(url, headers=headers)
+
+print(response)
