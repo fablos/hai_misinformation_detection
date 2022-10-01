a134cdeb9e12368195b7b4ce225ced48f2cc7198
fablos <fabrizio.loscudo@unical.it>
2022-10-01 23:10:05 +0200

    none

--- src/events_collector.py
+++ src/events_collector.py
@@ -4,6 +4,6 @@ source = 'https://www.nytimes.com/article/ukraine-russia-war-timeline.html'
 
 headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
 
-response = requests.get(url, headers=headers)
+response = requests.get(source, headers=headers)
 
 print(response)
