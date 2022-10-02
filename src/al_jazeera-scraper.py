f5d9b03c1976e8dae1b2d7bb88c1a7e8ce90b514
Fabrizio LoScudo <fabrizio.loscudo@unical.it>
2022-10-02 22:04:19 +0200

    al jaz

--- src/al_jazeera-scraper.py
+++ src/al_jazeera-scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+from keybert import KeyBERT
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
 
@@ -11,23 +11,36 @@ url = '/news/2022/9/29/russia-ukraine-war-list-of-key-events-day-218'
 
 def get_article(url='', base_url=BASE_URL, headers=HEADERS):
     response = requests.get(base_url + url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    article_content = soup.find('div', {'class': 'wysiwyg wysiwyg--all-content css-ibbk12'})
+    article_content = BeautifulSoup(response.content, 'html.parser').find('div', {'class': 'wysiwyg '
+                                                                                           'wysiwyg--all-content '
+                                                                                           'css-ibbk12'})
     return '\n'.join([p.text for p in BeautifulSoup(str(article_content), 'html.parser').findAll('p')])
+    #return [p.text for p in BeautifulSoup(str(article_content), 'html.parser').findAll('p')]
 
 
 def get_key_points(url='', base_url=BASE_URL, headers=HEADERS):
     response = requests.get(base_url + url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
+    events = []
+    kw_model = KeyBERT()
     for h in soup.find('ul', {'class': 'spaced-bullet-spacing'}):
         if len(h.text) > 1:
-            print(h.text, '\n')
+            o = {'short_text': h.text, 'articles': []}
+            #print(h.text, '\n')
             s2 = BeautifulSoup(str(h), 'html.parser')
             for link in s2.findAll('a'):
-                print(link.get('href'))
+                #print(link.get('href'))
                 article = get_article(url=link.get('href'))
-                print(article)
-                print('ooooooooooooooooooooooooooooooooooooooooooooooooooooooooo')
-            break
+                o['articles'].append({'body': article, 'keywords':kw_model.extract_keywords(article)})
 
-get_key_points(url=url)
+            events.append(o)
+    return events
+
+events = get_key_points(url=url)
+
+for e in events:
+    print(e['short_text'])
+    if len(e['articles']) > 0:
+        for a in e['articles']:
+            print(a['keywords'])
+    print('\n ****************************** \n')
